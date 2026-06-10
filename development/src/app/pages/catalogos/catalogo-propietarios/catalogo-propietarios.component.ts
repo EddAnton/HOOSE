10c122f71b4023f7a82722522cfe3111d8e1689f
EddAnton <tu-correo@ejemplo.com>
2026-06-10 12:14:57 -0600

    fix: quitar return suelto que bloqueaba edición de Propietarios sin unidades disponibles

--- development/src/app/pages/catalogos/catalogo-propietarios/catalogo-propietarios.component.ts
+++ development/src/app/pages/catalogos/catalogo-propietarios/catalogo-propietarios.component.ts
@@ -130,7 +130,6 @@ export class CatalogoPropietariosComponent implements OnInit {
 		    hlpSwal.Error('No hay unidades disponibles para asignar a un propietario.');
 		    return;
 		  }
-			return;
 		}
 
 		if (idUsuario > 0) {
